3f3911a4f0d3caf20b7d954323803219aa38aa88
Raniere Soares <ranierepsa@gmail.com>
2020-09-23 15:10:01 -0300

    Adding initial game assets and configurations
    Adding initial game assets and configurations

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 0.5f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 20f;
+    [SerializeField] float projectileFiringPeriod = 0.1f;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+    Coroutine fireCoroutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            fireCoroutine = StartCoroutine(FireContinuously());
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(fireCoroutine);
+        }
+    }
+
+    IEnumerator FireContinuously()
+    {
+        while (true)
+        {
+            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            yield return new WaitForSeconds(projectileFiringPeriod);
+        }
+    }
+
+    private void Move()
+    {
+        float horizontalMoveSpeed = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float verticalMoveSpeed = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x + horizontalMoveSpeed, xMin, xMax), Mathf.Clamp(transform.position.y + verticalMoveSpeed, yMin, yMax));
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera camera = Camera.main;
+        xMin = camera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = camera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        yMin = camera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = camera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+}
